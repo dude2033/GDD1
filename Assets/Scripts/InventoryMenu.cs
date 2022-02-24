cab959dfabe6ed50550d521bc7f973f50e374245
Daniel Marogy <daniel.marogy@student.tugraz.at>
2022-02-24 18:37:47 +0100

    Finished inventory

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   public GameObject test;
+    private void Start() {
+        
+        test = GameObject.FindGameObjectWithTag("Inventory");
+
+        for(int i = 0 ; i < test.transform.childCount; i++)
+            test.transform.GetChild(i).gameObject.SetActive(true);
+
+    }
+
+    
+
+    // Update is called once per frame
+
+}
