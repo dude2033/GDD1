964bc9a6833619b746b8e6559f115b155560435d
Burim Vrella <burimvrella@gmail.com>
2022-02-24 18:53:00 +0100

    Auto stash before checking out "origin/Develop"

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    private Renderer render;
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        render.material.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        render.material.color = Color.white;
+    }
+}
